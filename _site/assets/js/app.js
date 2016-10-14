82e57863315601ebbb63bb8d12bd7588ea0c1929
dumuarier <joshvogt@gmail.com>
2016-10-13 23:16:16 -0400

    Works Static Test
    Initial commit of worko static generator

---
+++ _site/assets/js/app.js
@@ -0,0 +1,30 @@
+/*********************************************************************************
+ Name: Worko Static
+ Dependencies:
+ Description: Worko Static Test.
+**********************************************************************************/
+
+var Workopolis = Workopolis || {};
+
+
+ (function (Workopolis, $) {
+    'use strict';
+
+    $(document).ready(function () {
+        Workopolis.Static.init();
+    });
+
+    var Static = {
+
+        analyticsInfo: {},
+
+        /******************* FUNCTION DEFINITIONS *********************/
+        init: function() {
+
+        },
+
+    };
+
+    Workopolis.Static = Static;
+
+}(Workopolis || {}, jQuery));
